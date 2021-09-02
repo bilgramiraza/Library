0b335ec9398eb9b73ebca0ac1c19c6e41cd6b594
Raza Hassan <bilgramiraza@yahoo.com>
2021-09-02 11:41:47 +0530

    Refactored JS file

--- script.js
+++ script.js
@@ -4,75 +4,126 @@ function Book(title, author, pageCount, isRead) {
 	this.pageCount=pageCount;
 	this.isRead=isRead;
 }
-
 let library=[];
-library.push(new Book('The Lord of the Rings','J.R.R Tolkien',250,false));
-library.push(new Book('The hobbit','J.R.R Tolkien',100,true));
-library.push(new Book('The cat in the hat','Dr. Suess',35,false));
-library.push(new Book('Sherlock Holmes','Sir Arthur Conan Doyle',175,true));
+
+window.onload=init();
+
+function init() {
+    const addBtn=document.querySelector('#add');
+    const delBtn=document.querySelector('#remove');
+    const bookShelf=document.querySelector('.bookShelf');
+    addBtn.addEventListener('click',addBookHandling);
+    delBtn.addEventListener('click',delBookHandling);
+    bookShelf.addEventListener('click',(event)=>{
+        displayBookHandling(event.target.textContent);
+    });
+    loadLibraryHandling();
+    displayAll();
+    displayBook(library[0]);
+}
+
+function addBookHandling() {
+    if(library.length===18){
+        alert('Shelf Full');
+        return;
+    }
+    toggleVisibility();
+    const submitBtn=document.querySelector('#submit');
+    submitBtn.addEventListener('click',addBook,{once:true});
+}
+function addBook(){
+    const inputs=document.querySelectorAll('input');
+    library.push(new Book(inputs[0].value,
+                        inputs[1].value,
+                        parseInt(inputs[2].value,10),
+                        inputs[3].checked));
+    toggleVisibility();
+    displayAll();
+}
+function toggleVisibility() {
+    const displayWindow=document.querySelector('.display');
+    const inputWindow=document.querySelector('.input');
+    displayWindow.classList.toggle('hide');
+    inputWindow.classList.toggle('hide');
+}
+
+function delBookHandling() {
+    if(library.length===0){
+        alert("Shelf Empty");
+        return;
+    }
+    const title=document.querySelector('#title');
+    const index=lookUp(title.textContent);
+    if(index===-1)
+        return;
+    clearBoard();
+    delBook(index);
+}
+function delBook(position) {
+    let libraryLeft=library.slice(0,position);
+    let libraryRight=library.slice(position+1);
+    library=[...libraryLeft,...libraryRight];
+    displayAll();
+}
 
 function displayAll() {
-    const shelf=document.querySelector('.bookShelf');
+    const bookShelf=document.querySelector('.bookShelf');
+    if(bookShelf.lastElementChild!==null)
+        clearBoard();
     library.forEach((book)=>{
-        let item=document.createElement('div');
-        item.textContent=book.title;
-        item.classList.add('book');
-        if(book.isRead)
-            item.classList.add('read');
-        else
-            item.classList.add('notread');
-        shelf.appendChild(item);
-    })
+        let element=createElement(book);
+        bookShelf.appendChild(element);    
+    });
 }
-displayAll();
-
-document.querySelector('.bookShelf').addEventListener('click',(event)=>{
-    const book=lookup(event.target.textContent);
-    display(book)
-})
-
-function lookup(title) {
-    for(book of library){
-        if(book.title===title)
-            return book;
-    }
+function createElement(obj) {
+    const item=document.createElement('div');
+    item.textContent=obj.title;
+    item.classList.add('book');
+    if(obj.isRead)
+        item.classList.add('read');
+    else
+       item.classList.add('notread');
+    return item;
+}
+function clearBoard() {
+    const parentNode=document.querySelector('.bookShelf');
+    while(parentNode.lastChild)
+        parentNode.removeChild(parentNode.lastChild);
 }
 
-function display(obj) {
+function displayBookHandling(title) {
+    const index=lookUp(title);
+    if(index===-1)
+        return;
+    displayBook(library[index]);
+}
+function displayBook(book) {
     const title=document.querySelector('#title');
     const author=document.querySelector('#author');
     const pages=document.querySelector('#pages');
     const read=document.querySelector('#read');
-    title.textContent=obj.title;
-    author.textContent=obj.author;
-    pages.textContent=obj.pageCount;
-    read.textContent=obj.isRead;
+    title.textContent=book.title;
+    author.textContent=book.author;
+    pages.textContent=book.pageCount;
+    if(book.isRead)
+        read.textContent='Read';
+    else
+        read.textContent='Not Read';
 }
 
-const input=document.querySelector('#add');
-input.addEventListener('click',()=>{
-    // document.querySelector('.display').classList.add('hide');
-    document.querySelector('.input').classList.remove('hide');
-    document.querySelector('#submit').addEventListener('click',addBook);
-})
-
-function addBook() {
-    const input=document.querySelector('#inputTitle');
-    const Author=document.querySelector('#inputAuthor');
-    const Pages=document.querySelector('#inputPages');
-    const Read=document.querySelector('#inputRead');
-    library.push(new Book(input.value,Author.value,Pages.value,Read.checked));
-    displayAll();
+function lookUp(title) {
+    return library.map((book)=>{return book.title})
+                  .indexOf(title);
 }
 
-const remove=document.querySelector('#remove');
-remove.addEventListener('click',()=>{
-    const toBeRemoved=document.querySelector('#title').textContent;
-    let index = library.map((book)=>{
-        return book.title;
-    }).indexOf(toBeRemoved);
-    let library1=(library.slice(0,index));
-    let library2=(library.slice(index+1));
-    library=[...library1,...library2];
-    displayAll();
-})
+function loadLibraryHandling() {
+    //TODO: Local data storage implementation
+    loadDummyData();
+}
+
+function loadDummyData() {
+    library.push(new Book('The Lord of the Rings','J.R.R Tolkien',250,false));
+    library.push(new Book('The hobbit','J.R.R Tolkien',100,true));
+    library.push(new Book('The cat in the hat','Dr. Suess',35,false));
+    library.push(new Book('Sherlock Holmes','Sir Arthur Conan Doyle',175,true));
+}
